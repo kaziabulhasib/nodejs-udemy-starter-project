82b14963c3c7236548c0c17d55f8afe3a39c24e3
kaziabulhasib <hasibkazi420@gmail.com>
2024-09-24 22:40:35 +0530

    createing our first server

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 const fs = require("fs");
 const http = require("http");
-const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
-console.log(textIn);
+// const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
+// console.log(textIn);
 
-const textOut = `This is what we know about avocado: ${textIn}. \n created at ${Date.now()}`;
+// const textOut = `This is what we know about avocado: ${textIn}. \n created at ${Date.now()}`;
 
-fs.writeFileSync("./txt/output.txt", textOut);
+// fs.writeFileSync("./txt/output.txt", textOut);
 
-fs.writeFileSync;
+// fs.writeFileSync;
 
-console.log("file is written");
+// console.log("file is written");
+
+// server
+
+const server = http.createServer((req, res) => {
+  res.end("Hello from the server");
+});
+
+server.listen(8000, "127.0.0.1", () =>
+  console.log("serverr is running on port 8000")
+);
